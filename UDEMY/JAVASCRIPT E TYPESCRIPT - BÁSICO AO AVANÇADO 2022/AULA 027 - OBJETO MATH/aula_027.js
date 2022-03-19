24e7ca85374f3c3d0fce46349afc69dc35f0b8a6
TabGuima <tab.guimaraes@gmail.com>
2022-03-19 20:58:35 -0300

    Ajustes gerais nos arquivos

---
+++ UDEMY/JAVASCRIPT E TYPESCRIPT - BÁSICO AO AVANÇADO 2022/AULA 027 - OBJETO MATH/aula_027.js
@@ -0,0 +1,28 @@
+// EXEMPLOS DE USO DO OBJETO 'MATH'
+
+let numeroComPontoFlutuante = 152.6546548741;
+let arredondaCima = Math.ceil(numeroComPontoFlutuante); // Arredonda para cima
+let arredondaBaixo = Math.floor(numeroComPontoFlutuante); // Arredonda para baixo
+let maisProximo = Math.round(numeroComPontoFlutuante); // Arredonda para o mais próximo do valor
+
+console.log();
+console.log(arredondaCima);
+console.log();
+console.log(arredondaBaixo);
+console.log();
+console.log(maisProximo);
+console.log();
+
+console.log(Math.max(301, 4454, 6545, 1231, 541, 89)); // Mostra o maior valor da lista
+console.log();
+console.log(Math.min(301, 4454, 6545, 1231, 541, 89)); // Mostra o menor valor da lista
+console.log();
+
+console.log(Math.round(Math.random() * 100)); // Retorna um número aleatório de acordo com o valor da multiplicação do segundo número
+console.log();
+
+console.log(Math.PI); // Retorna o valor de Pi
+console.log();
+
+console.log(144 ** 0.5); // Raíz quadrada= número ** 0.5
+console.log();
