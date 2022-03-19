120958de496df26f38ab55eed89e159b02aee75b
TabGuima <tab.guimaraes@gmail.com>
2022-03-19 14:38:29 -0300

    AULA 16 - RESOLUÇÃO DE EXERCÍCIO

---
+++ UDEMY/JAVASCRIPT E TYPESCRIPT - BÁSICO AO AVANÇADO 2022/AULA 16 - EXERCÍCIOS/app.js
@@ -0,0 +1,14 @@
+//Aula 16 - Exercícios com 'const' e 'let'
+
+const nome = 'Tiago';
+const sobreNome = 'Guimarães';
+const idade = 41;
+const peso = 79;
+const alturaEmM = 1.9;
+
+let imc = peso / (alturaEmM * alturaEmM);
+let anoNascimento = 2022 - idade;
+
+console.log(
+    `O IMC de ${nome} é de ${imc}.\nO ano de nascimento é ${anoNascimento}.\nA idade atual é de ${idade} anos.`
+);
