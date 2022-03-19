24aa270d267322b9cc7a8f96c1cca3013823bd24
TabGuima <tab.guimaraes@gmail.com>
2022-03-19 20:25:57 -0300

    alteração de nome de pasta e inclusão de exemplos de código

---
+++ UDEMY/JAVASCRIPT E TYPESCRIPT - BÁSICO AO AVANÇADO 2022/AULA 027 - OBJETO MATH/app.js
@@ -0,0 +1,10 @@
+// EXEMPLOS DE USO DO OBJETO 'MATH'
+
+let numeroComPontoFlutuante = 152.6546548741;
+let arredondaCima = Math.ceil(numeroComPontoFlutuante); // Arredonda para cima
+let arredondaBaixo = Math.floor(numeroComPontoFlutuante); // Arredonda para baixo
+let maisProximo = Math.round(numeroComPontoFlutuante); // Arredonda para o mais próximo do valor
+
+console.log(arredondaCima);
+console.log(arredondaBaixo);
+console.log(maisProximo);
