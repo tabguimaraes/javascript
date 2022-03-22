b75005127f271733aa588f73460e2fb8386c6063
TabGuima <tab.guimaraes@gmail.com>
2022-03-22 13:01:43 -0300

    primeira versão do código

--- UDEMY/JAVASCRIPT E TYPESCRIPT - BÁSICO AO AVANÇADO 2022/OBJETO DATE/app.js
+++ UDEMY/JAVASCRIPT E TYPESCRIPT - BÁSICO AO AVANÇADO 2022/OBJETO DATE/app.js
@@ -1,3 +1,21 @@
 //OBJETO DATE
 // REFERÊNCIA PARA CONSULTA:
 // https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Date
+
+function saudacao() {
+	let data = new Date();
+	let hora = data.getHours;
+
+	if (hora >= 0 && hora <= 12) {
+		console.log('Bom dia');
+	} else if (hora > 12 && hora <= 18) {
+		console.log('Boa tarde');
+	} else {
+		console.log('Boa noite');
+	}
+}
+
+saudacao();
+let data = new Date();
+let hora = data.getHours;
+console.log(hora);
