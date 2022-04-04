598100539de5f1ca546b7f90a17ad26c9b3fe7c6
TabGuima <tab.guimaraes@gmail.com>
2022-04-04 07:53:23 -0300

    add header and reference

--- UDEMY/JAVASCRIPT E TYPESCRIPT - BÁSICO AO AVANÇADO 2022/UNIDADE 02/ESTRUTURAS DE REPETIÇÃO/for_in.js
+++ UDEMY/JAVASCRIPT E TYPESCRIPT - BÁSICO AO AVANÇADO 2022/UNIDADE 02/ESTRUTURAS DE REPETIÇÃO/for_in.js
@@ -1,11 +1,28 @@
-//
+/*
+for...in
+O laço for...in  interage sobre propriedades enumeradas de um objeto, na ordem original de inserção.  O laço pode ser executado para cada propriedade distinta do objeto. 
 
-//
+(Resumo: For in -> Lê os indices e chaves do objeto)
 
-//
+Referência: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Statements/for...in
+
+*/
 
 frutas = ['maçã', 'uva', 'abacaxi'];
 
 for (indice in frutas) {
 	console.log(frutas[indice]);
+}
+console.log('//');
+console.log('//');
+console.log('//');
+
+//
+
+//Objeto
+var obj = { a: 1, b: 2, c: 3 };
+
+//Para prop (propriedade) in obj (objeto) faça
+for (var prop in obj) {
+	console.log('obj.' + prop + ' = ' + obj[prop]);
 }
