5fad59d9ebc29c85f44798fa47d1ede4a114c452
TabGuima <tab.guimaraes@gmail.com>
2022-04-21 13:37:36 -0300

    codigo final

--- DIO/DESAFIOS BOOTCAMP/DESAFIOS INTERMEDIARIOS/ordenar_vetor.js
+++ DIO/DESAFIOS BOOTCAMP/DESAFIOS INTERMEDIARIOS/ordenar_vetor.js
@@ -4,21 +4,24 @@ DADO UM ARRAY, CRIAR UM LAÇO PARA PERCORRER TODO O VETOR E VERIFICAR SE O VALOR
 
 SE FOR, IGNORAR. CASO CONTRÁRIO, ADICIONAR O NÚMERO DENTRO DO ARRAY E ORDENÁ-LO DO MENOR PARA O MAIOR.
 
-AO FINAL, MOSTRAR A LISTAGEM ORDENADA EM UM CONSOLE
+AO FINAL, MOSTRAR A LISTAGEM COMPLETA E ORDENADA EM UM CONSOLE E UMA LISTA COM OS NÚMEROS FALTANTES
 
 */
 
 let vetorNumeros = [
-	0, 1, 2, 3, 4, 5, 6, 7, 10, 11, 12, 14, 15, 16, 17, 18, 19, 20, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36,
-	39, 40, 41, 42, 43, 44, 45, 46, 51, 52, 57, 58, 59, 60, 61, 62, 63, 66, 77, 78, 85, 86, 87, 88, 89, 90, 91, 92, 93,
-	94, 95, 96, 97, 98, 99, 100,
+	1, 2, 3, 4, 5, 6, 7, 10, 11, 12, 14, 15, 20, 24, 25, 26, 27, 28, 29, 30, 34, 35, 36, 39, 40, 41, 42, 45, 46, 51,
+	57, 58, 59, 60, 66, 77, 78, 85, 86, 87, 88, 89, 90, 94, 95, 98, 99, 100,
 ];
 
-for (let i = 0; vetorNumeros.length < 100; i++) {
+let numerosAdicionados = [];
+
+console.log('========================================');
+console.log(`\nExistem ${vetorNumeros.length} números dentro da lista inicial.`);
+
+for (let i = 0; vetorNumeros.length <= 100; i++) {
 	if (vetorNumeros.includes(i)) {
 		continue;
 	} else {
-		numerosAdicionados = [];
 		numerosAdicionados.push(i);
 		vetorNumeros.push(i);
 	}
@@ -27,8 +30,10 @@ vetorNumeros.sort(function (a, b) {
 	return a - b;
 });
 
+console.log(`\nLista completa e ordenada do menor para o maior:\n`);
 console.log(vetorNumeros);
-// console.log('==============');
-// console.log(numerosAdicionados);
+console.log('\n========================================\n');
+console.log(`Os números faltantes da lista original são:\n`);
+console.log(numerosAdicionados);
 
-//EM DESENVOLVIMENTO
+//
