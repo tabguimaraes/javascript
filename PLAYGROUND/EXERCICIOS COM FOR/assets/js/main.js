d092587cf51c707634a8cc0c93a326b44d04405e
TabGuima <tab.guimaraes@gmail.com>
2022-04-27 07:56:15 -0300

    envio inicial

---
+++ PLAYGROUND/EXERCICIOS COM FOR/assets/js/main.js
@@ -0,0 +1,8 @@
+console.log('Conexão ok');
+
+const elementos = [
+	{ tag: 'p', texto: 'Frase 1' },
+	{ tag: 'div', texto: 'Frase 2' },
+	{ tag: 'footer', texto: 'Frase 3' },
+	{ tag: 'section', texto: 'Frase 4' },
+];
