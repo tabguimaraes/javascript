49f3f8de884ef468a861b5769bc7adacbf122160
TabGuima <tab.guimaraes@gmail.com>
2023-12-16 14:44:24 -0300

    ajuste geral de código

--- DIO/DESAFIOS BOOTCAMP/DESAFIOS INTERMEDIARIOS/CALCULO DE IMPOSTO DE RENDA/assets/js/main.js
+++ DIO/DESAFIOS BOOTCAMP/DESAFIOS INTERMEDIARIOS/CALCULO DE IMPOSTO DE RENDA/assets/js/main.js
@@ -23,7 +23,7 @@ pessoa deve pagar de Imposto de Renda, segundo a tabela abaixo. */
 /* Lembre que, se o salário for R$ 3002.00, a taxa que incide é de 8% apenas
 sobre R$ 1000.00, pois a faixa de salário que fica de R$ 0.00 até R$ 2000.00
 é isenta de Imposto de Renda. No exemplo fornecido (abaixo), a taxa é de 8%
-sobre R$ 1000.00 + 18% sobre R$ 2.00, o que resulta em R$ 80.36 no total. O
+sobre R$ 1000.00 + 18% sobre R$ 2, o que resulta em R$ 80.36 no total. O
 valor deve ser impresso com duas casas decimais.
 - Entrada
 A entrada contém apenas um valor de ponto flutuante, com duas casas decimais.
@@ -33,9 +33,6 @@ de Renda, com duas casas após o ponto. Se o valor de entrada for menor ou
 igual a 2000, deverá ser impressa a mensagem "Isento". */
 
 function init() {
-  // Teste de conexão
-  console.log("Conexão Ok");
-
   const form = document.querySelector("#formSalario");
 
   form.addEventListener("submit", event);
@@ -66,48 +63,27 @@ function init() {
       taxaAte4_5K = 0.18,
       taxaMaior4_5K = 0.28;
 
-    let saldoAte3K, saldoAte4_5K, saldoMaior4_5K, imposto;
+    let saldoAte4_5K, saldoMaior4_5K, imposto;
 
     // FAIXA DE SALARIO ISENTO DE IMPOSTO
     if (salario <= salarioLivreDeImposto) {
-      // console.log('Salário isento de imposto.');
       gerarResultado("Salário isento de imposto");
 
       // SALARIO ENTRE 2000 E 3000 - FAIXA 1 DE CONTRIBUIÇÃO
     } else if (salario > salarioLivreDeImposto && salario <= salarioAte3K) {
-      saldoAte3K = salario - salarioLivreDeImposto;
-
-      imposto = saldoAte3K * taxaAte3K;
-      // console.log(`Imposto devido: R$ ${imposto.toFixed(2)}`);
+      imposto = (salario - salarioLivreDeImposto) * taxaAte3K;
       gerarResultado(`Imposto devido: ${imposto.toLocaleString("pt-BR", { style: "currency", currency: "BRL" })}`);
 
       // SALARIO ENTRE 3000 E 4500 - FAIXA 2 DE CONTRIBUIÇÃO
     } else if (salario > salarioAte3K && salario <= salarioAte4_5K) {
-      salario = salario - salarioLivreDeImposto;
-
-      salario = salario - debitoAte3K;
-      saldoAte3K = debitoAte3K * taxaAte3K;
-
-      saldoAte4_5K = salario * taxaAte4_5K;
-
-      imposto = saldoAte3K + saldoAte4_5K;
-
+      saldoAte4_5K = salario - salarioLivreDeImposto - debitoAte3K; 
+      imposto = debitoAte3K * taxaAte3K + saldoAte4_5K * taxaAte4_5K;
       gerarResultado(`Imposto devido: ${imposto.toLocaleString("pt-BR", { style: "currency", currency: "BRL" })}`);
 
       // SALARIO ACIMA DE 4500 - FAIXA 3 DE CONTRIBUIÇÃO
     } else {
-      salario = salario - salarioLivreDeImposto;
-
-      salario = salario - debitoAte3K;
-      saldoAte3K = debitoAte3K * taxaAte3K;
-
-      salario = salario - debitoAte4_5K;
-      saldoAte4_5K = debitoAte4_5K * taxaAte4_5K;
-
-      saldoMaior4_5K = salario * taxaMaior4_5K;
-
-      imposto = saldoAte3K + saldoAte4_5K + saldoMaior4_5K;
-
+      saldoMaior4_5K = salario - salarioLivreDeImposto - debitoAte3K - debitoAte4_5K;
+      imposto = debitoAte3K * taxaAte3K + debitoAte4_5K * taxaAte4_5K + saldoMaior4_5K * taxaMaior4_5K;
       gerarResultado(`Imposto devido: ${imposto.toLocaleString("pt-BR", { style: "currency", currency: "BRL" })}`);
     }
   }
